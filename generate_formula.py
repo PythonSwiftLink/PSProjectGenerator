36f946bb07588dc3b90593735c1ac2e4b6c81201
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2023-10-09 08:53:58 +0200

    formula generator

--- generate_formula.py
+++ generate_formula.py
@@ -10,12 +10,61 @@ version = sys.argv[1]
 sha = sys.argv[2]
 
 
-template_dir = "https://github.com/PythonSwiftLink/BrewFormulaCookie.git"
 
-context = {
-    "version": version,
-    "name_version": version.replace(".",""),
-    "sha": sha
-}
+template0 = """
+class PSProject < Formula
+  desc ""
+  homepage ""
 
-cookiecutter(template_dir, no_input=True, extra_context=context)
+  url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/{version}/PSProject.tar.gz"
+  version "{version}"
+  sha256 "{sha}"
+  license ""
+
+  def install
+    bin.install "PSProject"
+    end
+
+  test do
+    system "false"
+  end
+end
+"""
+
+template0 = """
+class PSProject{version_name} < Formula
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/{version}/PSProject.tar.gz"
+  version "{version}"
+  sha256 "{sha}"
+  license ""
+
+  def install
+    bin.install "PSProject"
+    end
+
+  test do
+    system "false"
+  end
+end
+"""
+
+# template_dir = "https://github.com/PythonSwiftLink/BrewFormulaCookie.git"
+
+# context = {
+#     "version": version,
+#     "name_version": version.replace(".",""),
+#     "sha": sha
+# }
+
+formula_lastest = template0.format(version=version, version_name="", sha=sha)
+
+formula_version = template0.format(version=version, version_name=f'AT{version.replace(".","")}', sha=sha)
+
+with open("homebrew_formula/PSProject.rb", "w") as f:
+    f.write(formula_lastest)
+
+with open(f"homebrew_formula/PSProject@{version}.rb", "w") as f:
+    f.write(formula_version)
