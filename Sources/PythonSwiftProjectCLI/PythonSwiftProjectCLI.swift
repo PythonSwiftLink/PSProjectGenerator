43326a05a421c18f4a212963c24b1ac52293420c
PythonSwiftLink <pythonswiftlink@gmail.com>
2025-01-27 09:30:21 +0100

    Update PythonSwiftProjectCLI.swift

--- Sources/PythonSwiftProjectCLI/PythonSwiftProjectCLI.swift
+++ Sources/PythonSwiftProjectCLI/PythonSwiftProjectCLI.swift
@@ -10,7 +10,7 @@ import ArgumentParser
 struct PythonSwiftProjectCLI: AsyncParsableCommand {
 	
 	static var configuration: CommandConfiguration = .init(
-		version: "0.0.9",
+		version: "0.0.8",
 		subcommands: [Kivy.self, SwiftUI.self]
 	)
 	
